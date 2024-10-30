f358a772c2b16f969efa4d4d69b2de27f9e54d59
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-30 16:43:51 +0500

    feat:two way binding

--- src/components/EventComonent.vue
+++ src/components/EventComonent.vue
@@ -1,25 +1,15 @@
-<template>
-    <!-- <button @click="test('click me')">Button 1</button>
-    <button v-on:click="test('click me')">Button 2</button>
-    <button v-on:dblclick="test('double click')">Button 3</button> -->
 
-    <button @click="test">increment</button>
-    <button @click="descrement">decrement</button>
-    <button @click="reset">reset</button>
-    <h1>{{count}}</h1>
-</template>
-
-<script>
-export default {
-    name:"EventComonent",
-   data(){
-    {
+<!-- <script>
+    export default {
+        name:"EventComonent",
+        data(){
+            {
         return {
             count:0
         }
     }
-   },
-    methods:{
+},
+methods:{
         test(){
             this.count= this.count+1
         },
@@ -30,5 +20,48 @@ export default {
             this.count=this.test=0
         }
     }
+}
+</script> -->
+<template>
+    <!-- <button @click="test('click me')">Button 1</button>
+    <button v-on:click="test('click me')">Button 2</button>
+    <button v-on:dblclick="test('double click')">Button 3</button> -->
+    <!-- <input type="text" v-model="count">
+<br>
+<br>
+    <button @click="test">increment</button>
+    <button @click="descrement">decrement</button>
+    <button @click="reset">reset</button>
+
+    <h1>{{count}}</h1> -->
+
+    <input type="text" placeholder="Enter user Name" v-model="name">
+    <br>
+    <br>
+    <label for="java">Java</label>
+    <input type="checkbox" name="java" id="java" value="java" v-model="technlogy">  
+    <label for="react">React</label>
+    <input type="checkbox" name="react" id="react" value="react" v-model="technlogy">
+
+    {{technlogy}}
+    {{name}}
+</template>
+
+
+
+<!-- Two way binding -->
+ <script>
+export default {
+    name:"EventCounter",
+    data(){
+        {
+            return {
+                name:null,
+                technlogy:[]
+            }
+        }
+    }
+       
+      
 }
 </script>
