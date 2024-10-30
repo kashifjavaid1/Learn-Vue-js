7e689c476d0b8d59456715731ee77c48c71613a7
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-30 15:10:48 +0500

    interpolution

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,9 +2,14 @@
   <h1>Hello Home Component Call</h1>
   <h1 id="title">{{title}}</h1>
   <h1>Hello {{ "Hello".length }}</h1>
-  <h1>Email: {{email}}</h1>
+  <!-- <h1>Email: {{email}}</h1>
   <h1>Name: {{name}}</h1>
-  <h1>Get Name: {{getName("kashif")}}</h1>
+  <h1>Get Name: {{getName("kashif")}}</h1> -->
+  <h1>InterPolation</h1>
+  <h3>Email: {{email}}</h3>
+<h2>UserName: {{ getUserName()?.name?.userName}}</h2>
+<h2>UserEmail: {{ getUserName()?.email}}</h2>
+  <h1></h1>
 </template>
 <script>
 export default {
@@ -12,15 +17,25 @@ export default {
   props:{
     title:String,
   },
-  data(){
-    return {
-      email:"kashif@gmail.com",
-      name:"Vue js",
-       getName:function($name){
-return $name
-       }
-    }
-  }
+  // data(){
+  //   return {
+  //     email:"kashif@gmail.com",
+  //     name:"Vue jsc",
+//        getName:function($name){
+// return $name
+//        }
+  //   }
+  // },
+  // methods:{
+  //   getUserName(){
+  //        return {
+  //         name :{
+  //           userName:"kashif",
+  //         },
+  //         email:this.email
+  //        }
+  //   }
+  // }
 }
 </script>
 
