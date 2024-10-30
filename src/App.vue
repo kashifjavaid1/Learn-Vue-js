ffae67440b523cc97519ea0cd6f54cb98c27c39c
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-30 14:44:52 +0500

    interpolation and component's

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Learing Vue-js</h1>
+ <HomeComponent title="kashif"/>
+</template>
+<script>
+import HomeComponent from './components/HomeComponent.vue';
+export default {
+ components:{
+  HomeComponent
+ }
+}
+</script>
+<style scoped>
+h1{
+  color: orange;
+}
+</style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Hello Home Component Call</h1>
+  <h1 id="title">{{title}}</h1>
+  <h1>Hello {{ "Hello".length }}</h1>
+  <h1>Email: {{email}}</h1>
+  <h1>Name: {{name}}</h1>
+  <h1>Get Name: {{getName("kashif")}}</h1>
+</template>
+<script>
+export default {
+  name:"HomeComponent",
+  props:{
+    title:String,
+  },
+  data(){
+    return {
+      email:"kashif@gmail.com",
+      name:"Vue js",
+       getName:function($name){
+return $name
+       }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: blue;
+}
+#title{
+  color: purple;
+  font-size: 50px;
+}
+</style>
