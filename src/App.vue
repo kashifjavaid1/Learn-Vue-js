d9e0e4ed6124264dc172031476eba32a00349505
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-31 16:07:25 +0500

    feat:dyanmic components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>Learing Vue-js</h1>
-  <EventComonent/>
+  <!-- <EventComonent/>
   <ChildComponent/>
-  <FormComponent/>
+  <FormComponent/> -->
+  <!-- <NodeComponent/>
+  <PhpComponent/> -->
+
+<button @click="tab='NodeComponent'">Node js</button>
+<button @click="tab='PhpComponent'">Php</button>
+<button @click="tab='FormComponent'">Form</button>
+<br>
+<br>
+  <component :is="tab"/>
+
 </template>
 <script>
-// import ChildComponent from './components/childComponent.vue';
-import EventComonent from './components/EventComonent.vue';
-// import FormComponent from './components/FormComponent.vue';
+import NodeComponent from './components/NodeComponent.vue';
+import PhpComponent from './components/PhpComponent.vue';
+
+import FormComponent from './components/FormComponent.vue';
 
 
 
 export default {
  components:{
-  EventComonent,
+  // EventComonent,
   // ChildComponent
-  // FormComponent
+  FormComponent,
+    NodeComponent,PhpComponent
+ },
+ data(){
+return {
+  tab:'NodeComponent'
+}
  }
 }
 </script>
---
+++ src/components/NodeComponent.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Node Component</h1>
+</template>
+<script>
+export default {
+    name:"NodeComponent"
+}
+</script>
